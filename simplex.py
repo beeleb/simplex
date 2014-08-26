78508101b3d3ca8df318ca42bfd9e9433bf531b3
Takayasu Oono <ontkys@gmail.com>
2014-08-27 06:24:50 +0900

    3次元の確率分布を表すsimplexを作ります とりあえずmatplotlib.triを使った三角形だけ書きました

---
+++ simplex.py
@@ -0,0 +1,15 @@
+﻿%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+
+corners = np.array([[0, 0], [1, 0], [0.5, 0.75**0.5]])
+triangle = tri.Triangulation(corners[:, 0], corners[:, 1])
+ref = tri.UniformTriRefiner(triangle)
+trimesh = ref.refine_triangulation(subdiv=2)
+# if subdiv==n, triangle is divided into 4**n area
+plt.figure(figsize=(6, 6))
+plt.triplot(trimesh)
+plt.axis('off')
+plt.axis('equal')
+plt.show()
